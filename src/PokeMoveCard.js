d0ad9a7feb1412c687d43d633c3ec8138759d19d
kllivecka <aklivecka@gmail.com>
2022-09-06 20:28:41 +0000

    Update PokeMoveCard.js and PokeMoves.js

--- src/PokeMoveCard.js
+++ src/PokeMoveCard.js
@@ -1,7 +1,12 @@
-const PokeMoveCard = () => {
-    
+import './PokeMoveCard.css'
+
+const PokeMoveCard = (props) => {
+const { id, move, type } = props
     return (
-        <li>PokeMoveCard Component</li>
+        <li className='poke-move-card'>
+            <h3>Move {id}</h3>
+            <h4 style={{padding:10}}>{move.toUpperCase()}</h4>
+            <h5>Type: {type}</h5></li>
     )
 }
 
--- src/PokeMoves.js
+++ src/PokeMoves.js
@@ -1,15 +1,19 @@
 import { moves } from './data'
+import PokeMoveCard from './PokeMoveCard'
 
 const PokeMoves = () => {
+    const newArray = [1, 2, 3].map((item => {
+        <div key = {`item+${item.id}`}>thing</div>
+    }))
+    
     return(<div> 
         <h1>PokeMoves</h1>
         <ul>
-        {moves.map(item => (
-            <li key = {item.id}>
-                {item.id}. {item.move}
-            </li>
+        {moves.map((item) => (
+            <PokeMoveCard key = {item.id} {...item}/>
         ))}            
         </ul>
+        {newArray}
     </div>)
 }
 
